83d1b0e1ca863aec6ad8e4c6bae878f561884195
DESKTOP-Q8290FB\Major <majorgking@gmail.com>
2025-03-20 16:59:55 +0900

    클라이언트 UI_StageBlock을 서브아이템을 상속받아 작업 하도록 수정

--- Assets/@Scripts/UI/SubItem/UI_StageBlock.cs
+++ Assets/@Scripts/UI/SubItem/UI_StageBlock.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class UI_StageBlock : UI_SubItem
 {
@@ -26,5 +28,33 @@ public class UI_StageBlock : UI_SubItem
     int _stage;
     UI_SelectStageScene _selectStageSceneUI;
 
-    
+    protected override void Awake()
+    {
+        base.Awake();
+
+        BindTexts(typeof(Texts));
+        BindButtons(typeof(Buttons));
+        BindObjects(typeof(GameObjects));
+        BindImages(typeof(Images));
+
+        GetButton((int)Buttons.SelectStageButton).gameObject.BindEvent(OnClickStageButton);
+    }
+
+    public void SetInfo(int stage, UI_SelectStageScene scene)
+    {
+        _stage = stage;
+        _selectStageSceneUI = scene;
+
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+
+    }
+
+    private void OnClickStageButton(PointerEventData evt)
+    {
+
+    }
 }
