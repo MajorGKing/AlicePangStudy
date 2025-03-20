6ea64db8882df3598981f6e252f295e1bfea6eb5
MajorGKing <majorgking@gmail.com>
2025-03-20 16:29:59 +0900

    클라이언트 UI_StageBlock 작업 중

---
+++ Assets/@Scripts/UI/SubItem/UI_StageBlock.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_StageBlock : UI_SubItem
+{
+    enum GameObjects
+    {
+        StageTileSpine,
+        ClearFlag,
+    }
+
+    enum Texts
+    {
+        StageText,
+    }
+
+    enum Buttons
+    {
+        SelectStageButton,
+    }
+    enum Images
+    {
+        Shadow
+    }
+    int _stage;
+    UI_SelectStageScene _selectStageSceneUI;
+
+    
+}
