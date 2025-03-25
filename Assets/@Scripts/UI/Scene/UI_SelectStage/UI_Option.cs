7dd4fb015439ac18b66e3a6a32e0df7a45d57808
DESKTOP-Q8290FB\Major <majorgking@gmail.com>
2025-03-25 17:21:33 +0900

    클라이언트 UI_Option 기능 구현

--- Assets/@Scripts/UI/Scene/UI_SelectStage/UI_Option.cs
+++ Assets/@Scripts/UI/Scene/UI_SelectStage/UI_Option.cs
@@ -1,8 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class UI_Option : UI_Popup
 {
+    enum GameObjects
+    {
+        BGMOnImage,
+        BGMOffImage,
+        EffectSoundOnImage,
+        EffectSoundOffImage,
+        Block,
+    }
 
+    enum Buttons
+    {
+        BGMButton,
+        EffectSoundButton,
+        CloseButton,
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        Managers.Sound.Play(Define.ESound.Effect, "Sound_ButtonMain");
+
+        BindObjects(typeof(GameObjects));
+        BindButtons(typeof(Buttons));
+
+        GetButton((int)Buttons.BGMButton).gameObject.BindEvent(OnClickBGMButton);
+        GetButton((int)Buttons.EffectSoundButton).gameObject.BindEvent(OnClickEffectSoundButton);
+
+        GetButton((int)Buttons.CloseButton).gameObject.BindEvent(OnClickCloseButton);
+
+        GetObject((int)GameObjects.Block).BindEvent(OnClickCloseButton);
+
+        BGMOnOffImage(Managers.Game.BGMOn);
+        EffectSoundOnOffImage(Managers.Game.EffectSoundOn);
+    }
+
+    public void SetInfo()
+    {
+
+    }
+
+    public void RefreshUI()
+    {
+        BGMOnOffImage(Managers.Game.BGMOn);
+        EffectSoundOnOffImage(Managers.Game.EffectSoundOn);
+    }
+
+    void BGMOnOffImage(bool bgmOn)
+    {
+        GetObject((int)GameObjects.BGMOnImage).SetActive(bgmOn);
+        GetObject((int)GameObjects.BGMOffImage).SetActive(!bgmOn);
+    }
+
+    void EffectSoundOnOffImage(bool effectSoundOn)
+    {
+        GetObject((int)GameObjects.EffectSoundOnImage).SetActive(effectSoundOn);
+        GetObject((int)GameObjects.EffectSoundOffImage).SetActive(!effectSoundOn);
+    }
+
+    #region EventHandler
+    void OnClickBGMButton(PointerEventData evt)
+    {
+        bool bgmOn = Managers.Game.BGMOn;
+        bgmOn = !bgmOn;
+        Managers.Game.BGMOn = bgmOn;
+
+        BGMOnOffImage(bgmOn);
+
+        if (!bgmOn)
+            Managers.Sound.Stop(Define.ESound.Bgm);
+        else
+            Managers.Sound.Play(Define.ESound.Bgm);
+    }
+
+    void OnClickEffectSoundButton(PointerEventData evt) 
+    {
+        bool effectSoundOn = Managers.Game.EffectSoundOn;
+        effectSoundOn = !effectSoundOn;
+        Managers.Game.EffectSoundOn = effectSoundOn;
+
+        EffectSoundOnOffImage(effectSoundOn);
+        if (!effectSoundOn)
+            Managers.Sound.Stop(Define.ESound.SubBgm);
+        else
+            Managers.Sound.Play(Define.ESound.SubBgm);
+    }
+
+    void OnClickCloseButton(PointerEventData evt)
+    {
+        Managers.Sound.Play(Define.ESound.Effect, "Sound_HomeButton");
+        Managers.UI.ClosePopupUI(this);
+        Managers.Game.SaveGame();
+    }
+    #endregion
 }
